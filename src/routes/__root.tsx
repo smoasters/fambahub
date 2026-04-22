c5efdf9ae37c74a55bcb795674d75e9f3615a27e
matagaralph <matagaralph@gmail.com>
2026-04-22 17:35:24 +0200

    feat: setup google analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,9 @@
+import '@primer/primitives/dist/css/functional/themes/light.css';
+import '@primer/primitives/dist/css/primitives.css';
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router';
-
-import appCss from '../styles.css?url';
+import { GoogleAnalytics } from 'tanstack-router-ga4';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import appCss from '@/styles.css?url';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -37,13 +39,21 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      data-color-mode='light'
+      data-light-theme='light'
+      data-dark-theme='dark'
+    >
       <head>
         <HeadContent />
       </head>
       <body className='antialiased font-sans'>
-        <nav className='h-15 border-b'></nav>
-        <ThemeProvider>{children}</ThemeProvider>
+        <GoogleAnalytics measurementId='G-P86CEWRLRM' />
+        <ThemeProvider>
+          <nav className='h-15 border-b border-b-default'></nav>
+          {children}
+        </ThemeProvider>
         <Scripts />
       </body>
     </html>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({ component: App });
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-4 pt-8'>
-      <Button>Railway</Button>
+      <Button variant='primary'>Railway</Button>
     </div>
   );
 }
